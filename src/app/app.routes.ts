3a88b00949d9cdd5db9a11e14251c7d588684116
jhorsax161616 <71660993@continental.edu.pe>
2025-12-18 17:56:52 -0500

    Adding the Users option to the Sidebar with a blank panel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,6 +18,7 @@ import { VideosComponent } from './pages/ui-elements/videos/videos.component';
 import { SignInComponent } from './pages/auth-pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/auth-pages/sign-up/sign-up.component';
 import { CalenderComponent } from './pages/calender/calender.component';
+import { UsersComponent } from './pages/users/users.component';
 
 export const routes: Routes = [
   {
@@ -101,6 +102,11 @@ export const routes: Routes = [
         path:'videos',
         component:VideosComponent,
         title:'Angular Videos Dashboard | TailAdmin - Angular Admin Dashboard Template'
+      },
+      {
+        path:'users',
+        component:UsersComponent,
+        title:'Gestión de Usuarios | ELOHIM SGE'
       },
     ]
   },
