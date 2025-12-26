de2320c59a0b005aac7c3baff33e9bb039e2d192
jhorsax161616 <71660993@continental.edu.pe>
2025-12-26 10:16:26 -0500

    Update employee component with new scheme

--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -1,18 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { PageBreadcrumbComponent } from '../../shared/components/common/page-breadcrumb/page-breadcrumb.component';
-import { EmployeeTableComponent } from '../../shared/components/tables/employee-table/employee-table.component';
+import { EmployeeTableComponent } from './components/employee-table/employee-table.component';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
+import { ModalService } from '../../shared/services/modal.service';
+import { ModalComponent } from '../../shared/components/ui/modal/modal.component';
 
 @Component({
-  selector: 'app-users',
+  selector: 'app-employees',
   imports: [
     CommonModule,
     PageBreadcrumbComponent,
     EmployeeTableComponent,
+    ModalComponent,
+    EmployeeFormComponent,
   ],
   templateUrl: './employees.component.html',
   styles: ``
 })
 export class EmployeesComponent {
+
+  constructor(public modal: ModalService) {}
+
+  isOpen = false;
+  openModal() { this.isOpen = true; }
+  closeModal() { this.isOpen = false; }
 
 }
